65dcebb01a4966c788410320d6d9740f3e08d273
Harish-vinayagam <vinhari2212@gmail.com>
2025-12-23 22:30:40 +0530

    added basic styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,34 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { Link } from "react-router-dom";
+
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-              <h1>FinOSS</h1>
-
-      </div>
+      <header className="navbar">
+        <div className="brand"> FinOSS</div>
+      </header>
       <a
   href="https://github.com/Harish-vinayagam/FinOSS.git"
   target="_blank"
   rel="noopener noreferrer"
-  className="github-star-btn"
+  className="github-str"
 >
   ⭐ Star on GitHub
 </a>
+<section className='land'>
+  <div className='desc'> A lightweight discovery layer on top of GitHub. Browse, filter, and find the perfect open-source tools for your next project—all in one clean interface.
+    <Link to="/explore" className='primary-btn'>Get Started</Link>
+
+  </div>
+</section>
 
 
     </>
+    
   )
 }
 
