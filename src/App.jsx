ffde6b0b6ee77531c9bf899231d66728155e0324
Harish-vinayagam <vinhari2212@gmail.com>
2025-12-23 15:05:52 +0530

    added: star on github

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+              <h1>FinOSS</h1>
+
+      </div>
+      <a
+  href="https://github.com/Harish-vinayagam/FinOSS.git"
+  target="_blank"
+  rel="noopener noreferrer"
+  className="github-star-btn"
+>
+  ⭐ Star on GitHub
+</a>
+
+
+    </>
+  )
+}
+
+export default App
